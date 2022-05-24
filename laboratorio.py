2eca5f304d99eea829bf7917928eb3def9d1c7ad
Jessie <jncontreras@espe.edu.ec>
2022-05-23 20:50:34 -0500

    Creando mis variables

--- laboratorio.py
+++ laboratorio.py
@@ -6,3 +6,6 @@ def ambiente():
     global estado 
     estado = [] 
     for i in range (3): # Rango 3 puesto que son tres entorno
+        ambientes = random.randint (0,2) # el rango osilan entre 0 a 2 ya que hay estarian los tres entornos
+        estado.append(ambientes) 
+        print (estado) #imprimir mi estado actual
